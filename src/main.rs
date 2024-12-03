bad04388e252756b93a15d0f24fb440097b39a0a
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-12-03 17:03:49 +0100

    Write trait for functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Display;
+
+trait  Dyr {
+    fn lager_hvilken_lyd(&self) -> String;
+    fn spiser(&self) -> String;
+    fn er_farlig(&self) -> bool;
 }
